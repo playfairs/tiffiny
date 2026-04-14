1abff9aa1bf6452808bfd78b8bb5c93558036579
Invra <identificationsucks@gmail.com>
2026-04-14 10:29:59 +1000

    chore(nix): refactor flake inputs and outputs

--- flake.nix
+++ flake.nix
@@ -1,39 +1,39 @@
 {
   description = "tiffiny - CLI Tool to convert MP3 files into Images using TIFF Headers and RAW Data.";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+  outputs = { nixpkgs, ... }:
+  let
+    forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+  in
+  {
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustc
+          cargo
+          rustfmt
+          clippy
+        ];
+      };
+    });
+
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in rec {
+      tiffiny = pkgs.rustPlatform.buildRustPackage {
+        pname = "tiffiny";
+        version = "0.1.0";
+
+        src = ./.;
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+      };
+      default = tiffiny;
+    });
   };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rustc
-            cargo
-            rustfmt
-            clippy
-          ];
-        };
-
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "tiffiny";
-          version = "0.1.0";
-
-          src = ./.;
-
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-        };
-
-        apps.default = flake-utils.lib.mkApp {
-          drv = self.packages.${system}.default;
-        };
-      });
 }
